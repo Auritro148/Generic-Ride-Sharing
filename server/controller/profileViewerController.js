45423c393051201d95c55deb6051e81244e59dcb
Auritro148 <auritrosarkar123@gmail.com>
2026-08-14 01:55:13 +0600

    another gift for skillissue

--- server/controller/profileViewerController.js
+++ server/controller/profileViewerController.js
@@ -1,11 +1,30 @@
-async function profileData(req,res) {
+
+const jwt =  require("jsonwebtoken");
+const pool = require("../config/dbConfig");
+
+async function profileData(req, res) {
+
+    const Header = req.headers.authorization || '';
+    const token = Header.split(" ").at(1);
+
+    const payload = jwt.decode(token);
+    
+    
+
+    const query_val = {
+        text: 'SELECT EMAIL ,AGE FROM TEST WHERE EMAIL = $1',
+        values: [payload.id]
+    }
+
+    const user = await pool.query(query_val);
+
 
     return res.json({
-        id : "112873",
-        name : "dummy_user"
+        email: user.rows[0].email,
+        age: user.rows[0].age
     })
-    
+
 }
 
 
-module.exports = {profileData};
+module.exports = { profileData };
